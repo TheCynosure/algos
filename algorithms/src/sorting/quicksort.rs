59d9431899252eeeabe507283e37b292c86cd29b
TheCynosure <jack.github@gmail.com>
2017-09-03 12:35:44 -0400

    Added in the ability to call string_alphabetical sort with a str instead of a string.
    There is still a problem, and I believe it resides in the String sorting method not sorting correctly.

--- algorithms/src/sorting/quicksort.rs
+++ algorithms/src/sorting/quicksort.rs
@@ -1,5 +1,6 @@
+use std::string;
+
 #[allow(dead_code)]
-//TODO: Build some default compare funcs for common types like u32 and Strings.
 //Compare func should return true if first is bigger.
 pub fn sort<T, F>(arr: &mut [T], compare_func: &F)
 where F: Fn(&T, &T) -> bool {
@@ -34,9 +35,12 @@ where F: Fn(&T, &T) -> bool {
     }
 }
 
+#[allow(dead_code)]
 //Compare Func usually return true if the first one is bigger, so here we want to return the smaller one /
 //first one alphabetically.
-pub fn string_alphabetic(str1: &String, str2: &String) -> bool {
+pub fn string_alphabetic<S: string::ToString>(str1: &S, str2: &S) -> bool {
+    let str1 = str1.to_string();
+    let str2 = str2.to_string();
     //Find if any of the characters in str1 are before str2's characters
     //in the alphabet.
     for c_tuple in str1.chars().zip(str2.chars()) {
@@ -54,44 +58,48 @@ pub fn string_alphabetic(str1: &String, str2: &String) -> bool {
     false
 }
 
-
 #[cfg(test)]
 mod sort_test {
     use super::*;
     
     #[test]
     fn sort_i32() {
-		//Sort a normal array
-		let mut test_vec = vec![1, 5, 7, 8, 2, 4];
-		sort(&mut test_vec[..], &|num1, num2|{ num1 > num2 });
-		assert_eq!(test_vec, vec![1, 2, 4, 5, 7, 8]);
+        //Sort a normal array
+        let mut test_vec = vec![1, 5, 7, 8, 2, 4];
+        sort(&mut test_vec[..], &|&num1, &num2|{ num1 > num2 });
+        assert_eq!(test_vec, vec![1, 2, 4, 5, 7, 8]);
     
-    	//Check on an unnsorted array with repeat elems
+        //Check on an unnsorted array with repeat elems
         let mut test_vec = vec![1, 5, 8, 2, 6, 1];
-        sort(&mut test_vec[..], &|num1, num2|{ num1 > num2 });   
+        sort(&mut test_vec[..], &|&num1, &num2|{ num1 > num2 });   
         assert_eq!(test_vec, vec![1, 1, 2, 5, 6, 8]);
 
-		//Check on a sorted array
+        //Check on a sorted array
         let mut test_vec = vec![1, 2, 3, 4, 5, 6];
-        sort(&mut test_vec[..], &|num1, num2|{ num1 > num2 });
+        sort(&mut test_vec[..], &|&num1, &num2|{ num1 > num2 });
         assert_eq!(test_vec, vec![1, 2, 3, 4, 5, 6]);
     }
 
-	#[test]
-	fn sort_reverse_u32() {
-		//Sort a normal array, but in reverse order.
-		//Todo this just make it return if num2 is bigger, instead of num1.
-		let mut test_vec = vec![1, 5, 7, 8, 2, 4];
-		sort(&mut test_vec[..], &|num1, num2|{ num2 > num1 });
-		assert_eq!(test_vec, vec![8, 7, 5, 4, 2, 1]);
-	}
+    #[test]
+    fn sort_reverse_u32() {
+        //Sort a normal array, but in reverse order.
+        //Todo this just make it return if num2 is bigger, instead of num1.
+        let mut test_vec = vec![1, 5, 7, 8, 2, 4];
+        sort(&mut test_vec[..], &|&num1, &num2|{ num2 > num1 });
+        assert_eq!(test_vec, vec![8, 7, 5, 4, 2, 1]);
+    }
 
-    //TODO: Make the sorting of the list work for Strings and &str.
-
-	#[test]
-	fn sort_string_test() {
+    #[test]
+    fn sort_string_test() {
         let mut test_vec = vec!["Cat".to_string(), "Dog".to_string(), "Antelope".to_string()];
         sort(&mut test_vec[..], &string_alphabetic);
         assert_eq!(test_vec, vec!["Antelope".to_string(), "Cat".to_string(), "Dog".to_string()]);
-	}
+    }
+
+    #[test]
+    fn sort_str_test() {
+        let mut test_vec = vec!["Cat", "Walrus", "Rhino", "Yak", "Zebra", "Bacterium"];
+        sort(&mut test_vec[..], &string_alphabetic);
+        assert_eq!(test_vec, vec!["Bacterium", "Cat", "Rhino", "Walrus", "Yak", "Zebra"]);     
+    }
 }
