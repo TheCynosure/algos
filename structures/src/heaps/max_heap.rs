9acb7e4cc2412d270d1271f5f22ff0bb54d6cc0d
TheCynosure <jack.github@gmail.com>
2017-09-13 16:06:55 -0400

    Added in pushing support.
    Along with support for pushing things into the heap I had to change how the function generic works.
    Instead of using a where at the top of every function, I now use one at the top of the impl. This specifies the type for the entire impl statement and means I don't have to
    keep typing it out. Also I made it so unimplemented tests will fail to make it more obvious what else
    I have to do.

--- structures/src/heaps/max_heap.rs
+++ structures/src/heaps/max_heap.rs
@@ -4,15 +4,15 @@ extern crate algorithms;
 //returns if the second parameter is larger than the first.
 //This is because this is how the sorting algorithms sort backwards, i.e. in the format of
 //a max heap.
-pub struct MaxHeap<T, F> {
+pub struct MaxHeap<T, F: Fn(&T, &T) -> bool> {
 	elems: Vec<T>,
 	size: usize,
 	comparator_function: F,
 }
 
-impl<T, F> MaxHeap<T, F> {
-	pub fn new(f: F) -> Self
-	where F: Fn(&T, &T) -> bool {
+impl<T, F> MaxHeap<T, F>
+where F: Fn(&T, &T) -> bool {
+	pub fn new(f: F) -> Self {
 		Self {
 			elems: Vec::new(),
 			size: 0,
@@ -22,8 +22,7 @@ impl<T, F> MaxHeap<T, F> {
 
 	//This method is based off of quicksorting the array before making it into a heap.
 	//Worst case this should create an array in n^2
-	pub fn quicksort_heapify(mut arr: Vec<T>, f: F) -> Self
-	where F: Fn(&T, &T) -> bool {
+	pub fn quicksort_heapify(mut arr: Vec<T>, f: F) -> Self {
 		algorithms::sorting::quicksort::sort(&mut arr[..], &f);
 		let len = arr.len();
 		Self {
@@ -35,8 +34,7 @@ impl<T, F> MaxHeap<T, F> {
 
 	//This method is based on the sorting method explained to me by my TA Aritra.
 	//It should sort the array worst case n log(n)
-	pub fn aritra_heapify(mut arr: Vec<T>, f: F) -> Self
-	where F: Fn(&T, &T) -> bool {
+	pub fn aritra_heapify(mut arr: Vec<T>, f: F) -> Self {
 		let len = arr.len();
 		let mut mh = MaxHeap {
 			elems: arr,
@@ -47,17 +45,26 @@ impl<T, F> MaxHeap<T, F> {
 		//Start at the end
 		for i in (0..len).rev() {
 			//While the child is greater than the parent.
-			let mut child_index = i;
-			while (mh.comparator_function)(&mh.elems[mh.get_parent(i)], &mh.elems[child_index]) {
-				let parent_index = mh.get_parent(i);
-				//Swap the Child and the parent.
-				mh.elems.swap(parent_index, child_index);
-				//Keep tracking our child index so we can check the next parent.
-				child_index = parent_index;
-			}		
+			mh.comp_p_and_c_swap(i);
+					
 		}
 
 		return mh;
+	}
+
+	//Takes in the index of a child.
+	//Compares parent and child and if the child is greater will swap the child and parent, 
+	//continues up the entire heap, returning the new child index.
+	fn comp_p_and_c_swap(&mut self, index: usize) -> usize {
+		let mut child_index = index; //Copy the Child Index.
+		while (self.comparator_function)(&self.elems[self.get_parent(child_index)], &self.elems[child_index]) {
+			let parent_index = self.get_parent(child_index);
+			//Swap the Child and the parent.
+			self.elems.swap(parent_index, child_index);
+			//Keep tracking our child index so we can check the next parent.
+			child_index = parent_index;
+		}
+		child_index
 	}
 
 	pub fn get_parent(&self, index: usize) -> usize {
@@ -82,6 +89,17 @@ impl<T, F> MaxHeap<T, F> {
 			return None;
 		}
 		Some((index * 2) + 2)
+	}
+
+	//Pushes an item onto the stack and returns the index.
+	pub fn push(&mut self, elem: T) -> usize {
+		self.elems.push(elem);
+		let len = self.elems.len();
+		self.comp_p_and_c_swap(len - 1)
+	}
+
+	pub fn length(&self) -> usize {
+		self.elems.len()
 	}
 }
 
@@ -111,25 +129,36 @@ mod test {
 
 	#[test]
 	fn push_test() {
-		//TODO: Push something onto the heap.
+		let mut test_heap = MaxHeap::aritra_heapify(vec![7, 8, 2, 9, 1, 10, 14, 8], |num1, num2| {num2 > num1});
+
+		assert_eq!(test_heap.push(1), test_heap.length() - 1);
+
+		test_heap.push(8);
+		test_heap.push(3);
+
+		assert_eq!(test_heap.length(), 11);
 	}
 
 	#[test]
 	fn delete_test() {
 		//TODO: Delete something from the heap by index.
+		assert!(false);
 	}
 
 	#[test]
 	fn clear_test() {
 		//TODO: Clear the current heap.
+		assert!(false);
 	}
 
 	#[test]
 	fn max_test() {
 		//TODO: Get the maximum element in the heap.
+		assert!(false);
 	}
 
 	fn search_test() {
 		//TODO: Find elements in the heap.
+		assert!(false);
 	}
 }
