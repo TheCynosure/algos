0b77801538fdaa971e5e85fba786b273410e11e9
TheCynosure <jack.github@gmail.com>
2017-09-13 08:47:27 -0400

    Aritra Sort is working.
    The sorting has been modified, I no longer think it is 2^n log(n). Now I believe it is n log(n)
    worst case. Either way now it is working atleast!

--- structures/src/heaps/max_heap.rs
+++ structures/src/heaps/max_heap.rs
@@ -34,26 +34,39 @@ impl<T, F> MaxHeap<T, F> {
 	}
 
 	//This method is based on the sorting method explained to me by my TA Aritra.
-	//It should sort the array worst case 2^n Log(n) I believe.
+	//It should sort the array worst case n log(n)
 	pub fn aritra_heapify(mut arr: Vec<T>, f: F) -> Self
 	where F: Fn(&T, &T) -> bool {
 		let len = arr.len();
-		let mh = MaxHeap {
+		let mut mh = MaxHeap {
 			elems: arr,
 			size: len,
 			comparator_function: f,
 		};
 	
-		let childless_nodes = (len as f64).log(2.0) as usize;
-		for i in (len - 1)..(len - childless_nodes) {
-
+		for i in (0..len).rev() {
+			//While the child is greater than the parent.
+			let mut child_index = i;
+			while (mh.comparator_function)(&mh.elems[mh.get_parent(i)], &mh.elems[child_index]) {
+				let parent_index = mh.get_parent(i);
+				//Swap the Child and the parent.
+				mh.elems.swap(parent_index, child_index);
+				//Keep tracking our child index so we can check the next parent.
+				child_index = parent_index;
+			}		
 		}
 
 		return mh;
 	}
 
 	pub fn get_parent(&self, index: usize) -> usize {
-		(index / 2) + (index % 2) - 1
+		let temp = (index / 2) + (index % 2);
+		//If they have a parent we should subtract 1 to get the parent.
+		if temp > 0 {
+			return temp - 1;
+		}
+		//Otherwise we should return 0 if they don't have a parent.
+		temp
 	}
 
 	pub fn get_left_child(&self, index: usize) -> Option<usize> {
@@ -82,6 +95,16 @@ mod test {
 		assert_eq!(test_heap.elems[test_heap.get_parent(6)], 5);
 
 		let test_heap = MaxHeap::quicksort_heapify(vec![1, 5, 7, 3, 2, 6, 4], |num1, num2| {num2 > num1});		
+		assert_eq!(test_heap.elems[test_heap.get_parent(1)], 7);
+	}
+
+	#[test]
+	fn aritra_test() {
+		//Heapify with a reverse sort.
+		let test_heap = MaxHeap::aritra_heapify(vec![7, 6, 5, 4, 3, 2, 1], |num1, num2| {num2 > num1});
+		assert_eq!(test_heap.elems[test_heap.get_parent(6)], 5);
+
+		let test_heap = MaxHeap::aritra_heapify(vec![1, 5, 7, 3, 2, 6, 4], |num1, num2| {num2 > num1});		
 		assert_eq!(test_heap.elems[test_heap.get_parent(1)], 7);
 	}
 }
